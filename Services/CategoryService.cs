063f1a251fb34a1f6d56b9db3291cff3f0deca77
Oriailigo <31573758+Oriailigo@users.noreply.github.com>
2023-03-20 21:02:32 -0300

    Add files via upload

---
+++ Services/CategoryService.cs
@@ -0,0 +1,30 @@
+using System.Text.Json;
+using static blazorappdemo.CategoryService;
+
+namespace blazorappdemo;
+
+public class CategoryService: ICategoryService{
+
+    private readonly HttpClient client;
+     private readonly JsonSerializerOptions option;
+
+    public CategoryService(HttpClient httpClient){
+        client=httpClient;
+        option=new JsonSerializerOptions{PropertyNameCaseInsensitive =true};
+      }  
+    public async Task<List<Category>?> Get(){
+        var response=await client.GetAsync("v1/Categories");
+        var content= await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        }
+        return JsonSerializer.Deserialize<List<Category>>(content,option);
+        
+    }
+
+}
+    public interface ICategoryService
+   {
+        Task<List<Category>?> Get();
+    }
---
+++ Services/ProductService.cs
@@ -0,0 +1,50 @@
+
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace blazorappdemo;
+
+public class ProductService: IProductService{
+
+    private readonly HttpClient client;
+     private readonly JsonSerializerOptions option;
+
+    public ProductService(HttpClient httpClient){
+        client=httpClient;
+        option=new JsonSerializerOptions{PropertyNameCaseInsensitive =true};      }  
+    public async Task<List<Product>?> Get(){
+        var response=await client.GetAsync("v1/products");
+        var content= await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        }
+        return JsonSerializer.Deserialize<List<Product>>(content,option);
+    }
+
+    public async Task Add(Product product){
+        var response=await client.PostAsync("v1/products", JsonContent.Create(product));
+        var content= await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        }
+    }
+
+    public async Task Delete(int productId){
+        var response=await client.DeleteAsync($"v1/products/{productId}");
+        var content= await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new ApplicationException(content);
+        } 
+    }
+
+}
+
+    public interface IProductService
+   {
+        Task<List<Product>?> Get();
+        Task Add(Product product);
+        Task Delete(int productId);
+    }
