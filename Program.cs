01c5a69f7b9fe74ff06ee23015f2ad05804d2053
Oriailigo <31573758+Oriailigo@users.noreply.github.com>
2023-03-20 21:01:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using blazorappdemo;
+using Blazored.Toast;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddBlazoredToast();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+var apiURL= builder.Configuration.GetValue<string>("apiUrl");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiURL) });
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+await builder.Build().RunAsync();
